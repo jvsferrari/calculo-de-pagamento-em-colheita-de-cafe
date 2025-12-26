93dcfe0c94eb17a1b40ac706fa2334950d90ed6b
jvsferrari <jvsferrari@gmail.com>
2025-12-26 20:17:20 -0300

    add data bank local storage

--- script.js
+++ script.js
@@ -1,16 +1,73 @@
-const table = [
+const DATABANK_NAME = "allData" 
+const nameField = document.getElementById("name");
+const pricePerField = document.getElementById("price");
+const latoesField = document.getElementById("latoes");
+const litersField = document.getElementById("liters");
+
+
+function readDatabase(){
+    return JSON.parse(localStorage.getItem(DATABANK_NAME)) || [];
+}
+
+function saveData(list) {
+    localStorage.setItem(data, JSON.stringify(list));
+}
+
+function addName(nameField){
+
+    const nameTyped = nameField.value
+
+    if(nameTyped ===""){
+        nameTyped = "Nome não inserido"
+    }
+
+    const table = readDatabase()
+
+    table.push({
+        names: nameTyped,
+        pricePer: 0,
+        latoes: 0,
+        liters: 0
+    });
+
+    saveData(table)
+}
+
+function getNumber(event, field, number){
+
+    if (isNaN(field.value)){
+        event.preventDefault();
+        alert("Digite um valor numérico!");
+        return
+    }
+
+    const number = parseInt(field.value);
+
+    const table = readDatabase()
+
+    const lastPerson = data[data.length - 1];
+
+    lastPerson[number] = parseInt(number)
+
+    saveData(table)
+}
+
+function calculateCost(priceTyped, latoesTyped, litersTyped){
+    const pricePerTyped = parseInt(pricePerField.value);
+    const latoesTyped = parseInt(latoesField.value);
+    const litersTyped = parseInt(litersField.value);
+    costCalculated = priceTyped/60 * (latoesTyped * 60 + litersTyped)
+    
+    if (isNaN(pricePerTyped)||isNaN(latoesTyped)||)
+
+    table.push({
+        costColumn: costCalculated
+    })
+
+data.forEach(function(name)
+    
+}
+
+const finalTable = [
     {namesColumn:"", costColumn:""}
 ]
-
-const nameField = document.getElementById("name")
-const pricePerField = document.getElementById("price");
-const latao = document.getElementById("")
-
-const nameTyped = nameField.value
-const priceTyped = parseFloat(pricePerField.value)
-const lataoTyped
-
-table.push({
-    namesColumn: name,
-    costColumn: cost,
-})
