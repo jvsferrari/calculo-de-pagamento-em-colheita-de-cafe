189c47e4bd45c1fff0b18ca5d8129a5e2bfbdbd7
jvsferrari <jvsferrari@gmail.com>
2025-12-26 17:50:41 -0300

    correct labels, declare main array and variables

---
+++ script.js
@@ -0,0 +1,16 @@
+const table = [
+    {namesColumn:"", costColumn:""}
+]
+
+const nameField = document.getElementById("name")
+const pricePerField = document.getElementById("price");
+const latao = document.getElementById("")
+
+const nameTyped = nameField.value
+const priceTyped = parseFloat(pricePerField.value)
+const lataoTyped
+
+table.push({
+    namesColumn: name,
+    costColumn: cost,
+})
